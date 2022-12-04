1a68e67801310ae74d1fcdcb68c78c478ead16d4
06066060606060 <xmicky+git@hotmail.fr>
2022-12-04 21:10:13 +0400

    img

--- config/elfinder.php
+++ config/elfinder.php
@@ -10,7 +10,7 @@ return [
     | The dir where to store the images (relative from public).
     |
     */
-    'dir' => ['uploads'],
+    'dir' => ['img'],
 
     /*
     |--------------------------------------------------------------------------
@@ -26,8 +26,7 @@ return [
     |    ]
     */
     'disks' => [
-         'uploads',
-         'public',
+        // 'uploads',
     ],
 
     /*
