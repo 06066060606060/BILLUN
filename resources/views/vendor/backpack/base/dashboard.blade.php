f3cf966b39794ff9c7e2c5ebce1db9494b439b7e
06066060606060 <xmicky+git@hotmail.fr>
2022-12-08 04:32:04 +0400

    code signalement

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -18,13 +18,13 @@
 
         <div x-data="{ tab: 'description' }" id="tab_wrapper"
             class="flex flex-col md:w-1/2 bg-[#111827] rounded-xl h-96 md:mr-4 mb-8 shadow-md shadow-black">
-            <div class="flex justify-around  border-b border-gray-500 py-2 ">
+            <div class="flex justify-around py-2 border-b border-gray-500 ">
                 <a :class="{ 'active': tab === 'description' }" @click.prevent="tab = 'description'" href="#">
-                    <h1 class="text-xs lg:text-lg font-bold text-center text-white">Derniers Sites Ajoutés</h1>
+                    <h1 class="text-xs font-bold text-center text-white lg:text-lg">Derniers Sites Ajoutés</h1>
                 </a>
                 @if (backpack_user()->role == 'admin')
                     <a :class="{ 'active': tab === 'reviews' }" @click.prevent="tab = 'reviews'" href="#">
-                        <h1 class="text-xs lg:text-lg font-bold text-center text-white">Derniers Sites Signalés</h1>
+                        <h1 class="text-xs font-bold text-center text-white lg:text-lg">Derniers Sites Signalés</h1>
                     </a>
                 @endif
             </div>
@@ -219,14 +219,14 @@
         @if (backpack_user()->role == 'admin')
             <div class="flex flex-col md:w-1/4 bg-[#111827] rounded-xl h-72 md:ml-4 shadow-md shadow-black mb-4">
                 <h1 class="py-2 text-xl font-bold text-center text-white border-b border-gray-500">Sites:</h1>
-                <div class="ct-chart z-0 flex mx-auto w-auto py-1">
+                <div class="z-0 flex w-auto py-1 mx-auto ct-chart">
                 </div>
                 <div class="flex justify-center">
-                    <li class="flex flex-row mx-2 pb-2">
+                    <li class="flex flex-row pb-2 mx-2">
                         <div class="w-4 h-4 bg-green-600 rounded-full"></div>
                         <div class="ml-2 text-sm text-gray-300">Sécurisé</div>
                     </li>
-                    <li class="flex flex-row mx-2 pb-2">
+                    <li class="flex flex-row pb-2 mx-2">
                         <div class="w-4 h-4 bg-red-600 rounded-full"></div>
                         <div class="ml-2 text-sm text-gray-300">Non Sécurisé</div>
                     </li>
@@ -234,14 +234,14 @@
             </div>
             <div class="flex flex-col md:w-1/4 md:pt-0 bg-[#111827] rounded-xl h-72 md:ml-4 shadow-md shadow-black">
                 <h1 class="py-2 text-xl font-bold text-center text-white border-b border-gray-500">Emails:</h1>
-                <div class="ct-chart2 z-0 flex mx-auto w-auto py-1">
+                <div class="z-0 flex w-auto py-1 mx-auto ct-chart2">
                 </div>
                 <div class="flex justify-center">
-                    <li class="flex flex-row mx-2 pb-2">
+                    <li class="flex flex-row pb-2 mx-2">
                         <div class="w-4 h-4 bg-green-600 rounded-full"></div>
                         <div class="ml-2 text-sm text-gray-300">Sécurisé</div>
                     </li>
-                    <li class="flex flex-row mx-2 pb-2">
+                    <li class="flex flex-row pb-2 mx-2">
                         <div class="w-4 h-4 bg-red-600 rounded-full"></div>
                         <div class="ml-2 text-sm text-gray-300">Non Sécurisé</div>
                     </li>
@@ -251,10 +251,10 @@
 
     </section>
     @if (backpack_user()->role == 'admin')
-        <p class="rounded text-xs py-2  bg-gray-900 text-gray-200 w-full px-4">
+        <p class="w-full px-4 py-2 text-xs text-gray-200 bg-gray-900 rounded">
              Derniéres mise à jour:  {{ $version }}<br>
-            -nettoyage des entrées http/https.<br>
-            -filtrage entrées unique.
+            -Ajout api signalement<br>
+            -Code signalement identification mail ou site.
         </p>
        
     @endif
